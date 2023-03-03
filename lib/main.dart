48d5b2d2fa388c54102a5ee59b430a42b0bffe66
Bourdais Rémi <remi.bourdais@telecomnancy.eu>
2023-03-03 19:18:35 +0000

    chapitre 1 à 5

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
         ),
         home: MyHomePage(),
       ),
@@ -27,19 +27,86 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var randommot = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RandomCard(randommot: randommot),
+            SizedBox(height: 20),
+            RandomButton(appState: appState)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RandomButton extends StatelessWidget {
+  const RandomButton({
+    super.key,
+    required this.appState,
+  });
+
+  final MyAppState appState;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+
+    return ElevatedButton(onPressed: ()  {
+      appState.getNext();
+    }, 
+      style: ElevatedButton.styleFrom(
+        backgroundColor: theme.colorScheme.onPrimary,
+        elevation: 10,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+      ),
+      child: Text('Cliquez ici'),
+    );
+  }
+}
+
+class RandomCard extends StatelessWidget {
+  const RandomCard({
+    super.key,
+    required this.randommot,
+  });
+
+  final WordPair randommot;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var style = theme.textTheme.displayLarge!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      shadowColor: theme.colorScheme.primary,
+      elevation: 10,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(randommot.asLowerCase, style: style),
       ),
     );
   }
